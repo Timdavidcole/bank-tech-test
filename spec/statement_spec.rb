3c4f43990bc05c383c7bcf87f2d26c4e1102ecff
timdavidcole <timdavidcole@gmail.com>
2019-08-27 14:13:37 +0100

    added statement test

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,4 +8,11 @@ describe Statement do
     end
   end
 
+  describe "#return_statement" do
+    it "creates the statement array" do
+      statement = Statement.new(2500, [[1000, "14/01/2012"], [2000, "13/01/2012"], [-500, "14/01/2012"]])
+      expect(statement.return_statement).to eq (["date || credit || debit || balance", "14/01/2012 || || 500 || 2500", "13/01/2012 || 2000 || || 3000", "14/01/2012 || 1000 || || 1000"])
+    end
+  end
+
 end
