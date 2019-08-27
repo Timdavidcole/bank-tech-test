b05d5526e95cfaed3e9928e064a99e81d94d12ea
timdavidcole <timdavidcole@gmail.com>
2019-08-27 13:21:15 +0100

    added statement function

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,18 +2,53 @@ class Bank
 
   def initialize()
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(amount)
+  def deposit(amount, date)
     @balance += amount
+    @transactions.push([amount, date])
   end
 
-  def withdraw(amount)
+  def withdraw(amount, date)
     @balance -= amount
+    @transactions.push([(amount * -1), date])
   end
 
   def show_balance
     @balance
   end
 
+  def print_statement
+    print_transactions
+  end
+
+  private
+
+  def print_transactions
+    statement_balance = @balance
+    statement = []
+    statement.push(statement_header)
+    @transactions.reverse_each do |transaction|
+      if transaction[0] < 0
+        statement.push("#{transaction[1]} || || #{transaction[0]*-1} || #{statement_balance}")
+        statement_balance -= transaction[0]
+      else
+        statement.push("#{transaction[1]} || #{transaction[0]} || || #{statement_balance}")
+        statement_balance -= transaction[0]
+      end
+    end
+    statement
+  end
+
+  def statement_header
+    "date || credit || debit || balance"
+  end
+
 end
+
+bank = Bank.new
+bank.deposit(1000, '14/01/2012')
+bank.deposit(2000, '13/01/2012')
+bank.withdraw(500, '14/01/2012')
+puts bank.print_statement
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -18,8 +18,8 @@ describe Bank do
 
   describe "#deposit" do
     it "allows a user to deposit money" do
-      subject.deposit(200)
-      expect(subject.show_balance).to eq 200
+      subject.deposit(1000, "10/01/2012")
+      expect(subject.show_balance).to eq 1000
     end
   end
 
@@ -29,8 +29,8 @@ describe Bank do
 
   describe "#deposit" do
     it "allows a user to withdraw money" do
-      subject.withdraw(200)
-      expect(subject.show_balance).to eq -200
+      subject.withdraw(500, '14/01/2012')
+      expect(subject.show_balance).to eq -500
     end
   end
 
@@ -40,13 +40,11 @@ describe Bank do
 
   describe "#print_statement" do
     it "allows a user to view all transactions" do
-      subject.withdraw(200)
-      expect(subject.show_balance).to eq -200
+      subject.deposit(1000, '14/01/2012')
+      subject.deposit(2000, '13/01/2012')
+      subject.withdraw(500, '14/01/2012')
+      expect(subject.print_statement).to eq (["date || credit || debit || balance", "14/01/2012 || || 500 || 2500", "13/01/2012 || 2000 || || 3000", "14/01/2012 || 1000 || || 1000"])
     end
   end
 
 end
-
-# As a user,
-# So that I can get an overview of my account,
-# I want to be able to print my statement.
